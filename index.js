6c8a848fad9cb344e0e6f93441d8d55bef5503da
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2023-09-14 17:24:11 +0530

    check via cors

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
+const cors=require("cors")
+server.use(cors())
 server.use(middlewares);
 server.use("", router);
 server.listen(process.env.PORT || 5000, () => {
